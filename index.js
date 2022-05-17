fdf78e6d2a9a8b5b5ebb6d1f85198ca203f89a76
Eric K. Kogi <eric.kogi@ncbagroup.com>
2022-05-17 20:57:31 +0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
 var ENC_DEC = require('./enc_dec');
 var enc_dec = new ENC_DEC();
-const server = http.createServer((req, res) => {
-    const msg = 'Started!\n'
-    res.end(msg);
+
+let encoded_file = "newecodedfile.enc";
+
+// ENCODE A FILE
+var inputFileName = 'rawfile.env';
+var outputFileName = 'newecodedfile.enc';
+enc_dec.encode(inputFileName, outputFileName, function (data) {
+    console.log("*************** Encoded output *****************");
+    console.log(data);
 });
 
-server.listen(port, () => {
-    let encoded_file = "newecodedfile.enc";
-
-    // ENCODE A FILE
-    var inputFileName = 'rawfile.env';
-    var outputFileName = 'newecodedfile.enc';
-    enc_dec.encode(inputFileName, outputFileName, function (data) {
-        console.log("*************** Encoded output *****************");
-        console.log(data);
-    });
-
-    // DECODE A FILE
-    enc_dec.decode(__dirname + '/' + encoded_file, function (data) {
-        console.log("*************** Decoded output *****************");
-        console.dir(data);
-    });
+// DECODE A FILE
+enc_dec.decode(__dirname + '/' + encoded_file, function (data) {
+    console.log("*************** Decoded output *****************");
+    console.dir(data);
 });
